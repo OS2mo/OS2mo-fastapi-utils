b806a00caa5a42a99c5ef6029b2b427eb0296e0d
Emil Madsen <sovende@gmail.com>
2021-03-16 14:50:31 +0100

    REUSE compliance

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-#
 # Copyright (c) 2017, Magenta ApS
+# SPDX-FileCopyrightText: Magenta ApS
 #
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
+# SPDX-License-Identifier: MPL-2.0
+
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
